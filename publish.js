31fa3aa7f3618b65dab954327513c43e62dfd766
swagc2 <taibaidded1122@gmail.com>
2025-11-11 09:10:21 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'xhgrrxot.cc',
+	'gjlhvxnf.cc',
 ];
                                                                                                                   
 var JumpPage="https://swagc.com";
